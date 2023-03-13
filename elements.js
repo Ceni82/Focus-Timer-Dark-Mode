26dae882b2c8c29894bf63d27da35ca0e89ba742
Mateus Ceni <98861755+Ceni82@users.noreply.github.com>
2023-03-13 11:02:28 -0300

    Add files via upload

---
+++ elements.js
@@ -0,0 +1,49 @@
+
+const buttonPlay = document.querySelector('#play');
+const buttonStop = document.querySelector('#stop')
+const buttonPause = document.querySelector('.pause')
+const buttonDown = document.querySelector ('#down')
+const buttonUp = document. querySelector ('#up')
+const buttonCards = document.querySelector('#cards')
+const buttonTree = document.querySelector('#soundTree')
+const buttonRains = document.querySelector('#soundRain')
+const buttonHouse = document.querySelector('#soundHouse')
+const buttonFire = document.querySelector('#soundFire')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+const buttonDark = document.querySelector('.modeDark')
+const buttonLight = document.querySelector('.modeLight')
+const timerControls = document.querySelector('#controls')
+const volTree = document.querySelector('#voltree')
+const volRain = document.querySelector('#volrain')
+const volHouse = document.querySelector('#volhouse')
+const volFire = document.querySelector('#volfire')
+
+
+
+
+export const elements = {
+    buttonPlay,
+    buttonPause,
+    buttonStop,
+    buttonDown,
+    buttonUp,
+    buttonTree,
+    buttonRains,
+    buttonHouse,
+    buttonFire,
+    minutesDisplay,
+    secondsDisplay,
+    buttonDark,
+    buttonLight,
+    buttonCards,
+    timerControls,
+
+    volTree,
+    volRain,
+    volHouse,
+    volFire
+
+    
+}
+
